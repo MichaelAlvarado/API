6674dca6357611f803bf8e51f8448eed1435625a
ubuntu <ubuntu@ubuntu-VirtualBox>
2019-10-21 17:42:51 -0400

    Testers added

--- src/main/java/edu/uprm/cse/datastructures/cardealer/model/CarList.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/CarList.java
@@ -6,26 +6,19 @@ import edu.uprm.cse.datastructures.cardealer.util.CircularSortedDoublyLinkedList
 import edu.uprm.cse.datastructures.cardealer.util.SortedList;
 
 public class CarList {
-private static SortedList<Car> cList = new CircularSortedDoublyLinkedList<Car>(new CarComparator());
-private static final ArrayList<Car> AList = new ArrayList<Car>();
+	private static SortedList<Car> cList = new CircularSortedDoublyLinkedList<Car>(new CarComparator());
 
-static {
-	AList.add(new Car(0,"test", "test", "test", 2000.00));
+	static {
+		cList.add(new Car(0, "test", "test", "test", 450.00));
+		cList.add(new Car(1, "test", "test", "test", 200.00));
+	}
+	private CarList() {}
 
-	
-	cList.add(new Car(0, "test", "test", "test", 450.00));
-	cList.add(new Car(1, "test", "test", "test", 200.00));
-}
-private CarList() {}
-public static SortedList<Car> getInstance(){
-	return cList;
-}
-public static ArrayList<Car> getAInstance(){
-	return AList;
-}
-public void resetCars() {
-	this.cList =  new CircularSortedDoublyLinkedList<Car>(new CarComparator());
-	
-}
+	public static SortedList<Car> getInstance(){
+		return cList;
+	}
+	public static void resetCars() {
+		cList =  new CircularSortedDoublyLinkedList<Car>(new CarComparator());	
+	}
 
 }
